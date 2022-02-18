058653f2ead25cd4a998234fb4abc0f91142464d
luftedar <ugurorcun91@gmail.com>
2022-02-18 20:09:04 +0300

    Fix Linters Erros

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    before(:example) { get '/users'}
+    before(:example) { get '/users' }
     it 'Successfuly get route' do
       expect(response).to have_http_status(:ok)
     end
