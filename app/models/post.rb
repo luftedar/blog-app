a8786e38eb570fdaf4324b93ed37cae3170fced4
luftedar <ugurorcun91@gmail.com>
2022-02-24 18:44:18 +0300

    Add Foreign Key For Post: user_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
 end
