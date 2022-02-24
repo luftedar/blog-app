e9f7a554c964c1fa0fca5c9b75a5147c059c088b
luftedar <ugurorcun91@gmail.com>
2022-02-24 19:00:23 +0300

    Add update_comments_counter for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
