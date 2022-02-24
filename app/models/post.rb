d3dfdc321ccc6cb7042cc4f50b8c37432c61115f
luftedar <ugurorcun91@gmail.com>
2022-02-24 18:58:35 +0300

    Add update_posts_counter for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
 
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
