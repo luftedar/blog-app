e5652fc25d81064baf481e6ec3f7790dba3cbc1b
luftedar <ugurorcun91@gmail.com>
2022-02-24 18:57:08 +0300

    Add load_last_three_posts for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :user_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
+
+  def load_last_three_posts
+    Post.order('created_at Desc').limit(3)
+  end
 end
