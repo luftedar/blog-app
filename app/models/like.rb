1ed619748286f042fe98adf02f5c0fbe8860b339
luftedar <ugurorcun91@gmail.com>
2022-02-25 10:21:45 +0300

    After Save For Update Like Counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
 
+  after_save :update_likes_counter
+
   private
 
   def update_likes_counter
