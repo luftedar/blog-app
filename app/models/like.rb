a3ee78ba32285b8c70ac8a8d912789ef323c1360
luftedar <ugurorcun91@gmail.com>
2022-02-28 14:05:18 +0300

    Add update_likes_counter for Like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,8 @@ class Like < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  def update_likes_counter
+    Post.increment!(:likes_counter)
+  end
 end
