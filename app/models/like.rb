b442df651458bf03669ba641f245b059011bc796
luftedar <ugurorcun91@gmail.com>
2022-02-28 15:35:46 +0300

    Implement Required Changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
-  has_many :likes
-  has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+  belongs_to :post
 
   after_save :update_likes_counter
 
