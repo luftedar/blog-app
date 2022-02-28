ce3ec4a7c59e2be7bbb5b66cb671338b2a647acd
luftedar <ugurorcun91@gmail.com>
2022-02-28 14:03:21 +0300

    Add recent_five_comments for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  def recent_five_comments
+    Comment.order('created_at Desc').limit(5)
+  end
 end
