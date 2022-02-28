851bccd98b4f51b6cf947c2fe101850f93f39ef8
luftedar <ugurorcun91@gmail.com>
2022-02-28 13:29:13 +0300

    Fix Author

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :likes,
-  has_many :comments,
-  belongs_to :user, foreign_key: :user_id
+  has_many :likes
+  has_many :comments
+  belongs_to :author,class_name: 'User', foreign_key: :user_id
 end
