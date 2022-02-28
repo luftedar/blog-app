df5434f7b53f2bf24d7dcb4c402203aa082e55a4
luftedar <ugurorcun91@gmail.com>
2022-02-28 12:30:01 +0300

    Final Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
+  has_many :posts, foreign_key: 'user_id'
+
 
   def load_last_three_posts
-    Post.last(3)
-  end
+    posts.limit(3).order(created_at: :desc)
+   end
 end
