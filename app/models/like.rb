613414da631b83438b5b5bb470e3c93ce2f1b4ac
luftedar <ugurorcun91@gmail.com>
2022-02-28 14:06:06 +0300

    Add after_save for Post, Like, Comment

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,8 @@ class Like < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+  after_save :update_likes_counter
+
   def update_likes_counter
     Post.increment!(:likes_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+  after_save :update_posts_counter
+
   def recent_five_comments
     Comment.order('created_at Desc').limit(5)
   end
