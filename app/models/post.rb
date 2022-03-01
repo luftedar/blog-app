c7b8fde55b6868e16cb8210a2d8ffa560e7395a3
luftedar <ugurorcun91@gmail.com>
2022-03-01 21:07:06 +0300

    Add user/show Page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,14 @@
 class Post < ApplicationRecord
-  has_many :likes
+  belongs_to :user
   has_many :comments
-  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  has_many :likes
 
   after_save :update_posts_counter
 
-  private
-
-  def recent_five_comments
-    Comment.order('created_at Desc').limit(5)
-  end
-
   def update_posts_counter
-    author.increment!(:posts_counter)
+    user.increment!(:posts_counter)
+  end
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
   end
 end
