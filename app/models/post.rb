783445ef31a201752e0b9d5537906380a67ab436
luftedar <ugurorcun91@gmail.com>
2022-03-02 16:58:05 +0300

    Render Posts Index

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,8 @@ class Post < ApplicationRecord
   def update_posts_counter
     user.increment!(:posts_counter)
   end
+  
   def recent_comments
-    comments.limit(5).order(created_at: :desc)
+    comments.order(created_at: :desc).limit(5)
   end
 end
