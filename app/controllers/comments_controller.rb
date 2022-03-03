f3dddb7233eda800009266874efd25bb2c395430
luftedar <ugurorcun91@gmail.com>
2022-03-03 17:30:54 +0300

    Add Create Action for Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,18 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @new_comment = current_user.comments.new(
+      text :params.require(:comment).permit(:text)[:text],
+      user_id :current_user.id,
+      post_id :@post.id
+    )
+    @new_comment.post_id = @post.id
+    @new_comment.update_comments_counter
+    if @new_comment.save
+      redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", notice: 'Posted Success!'
+    else
+      render :new, alert: 'Failed to Post the Comment'
+    end
   end
 end
