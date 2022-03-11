0c0f8686005c920b28237c592749498e8a8140f8
luftedar <ugurorcun91@gmail.com>
2022-03-11 14:45:39 +0300

    Fixed the user var name

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'Comment model' do
-    user = User.create(name: 'Orcun', photo: 'Cat.png', bio: 'Test Bio')
-    post = Post.new(user:, title: 'test post', text: 'Testing post')
-    comment = Comment.new(user:, post:, text: 'Test Comment')
+    user1 = User.create(name: 'Orcun', photo: 'Cat.png', bio: 'Test Bio')
+    post = Post.new(user: user1, title: 'test post', text: 'Testing post')
+    comment = Comment.new(user: user1, post:, text: 'Test Comment')
     comment.update_comments_counter
 
     it 'Check Comments Counter' do
       current = post.comments_counter
-      comment_new = Comment.new(user:, post:, text: 'New Test Comment')
+      comment_new = Comment.new(user: user1, post:, text: 'New Test Comment')
       comment_new.update_comments_counter
       expect(post.comments_counter).to eq(current + 1)
     end
